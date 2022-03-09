b90facb58581f33271aa35b353c998d159f4186a
Jessica <1561578081@qq.com>
2022-03-09 22:24:26 +0800

    修改了一次

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ var server = http.createServer(function(request, response){
     response.end()
   } else {
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`你访问的页面不存在`)
     response.end()
   }
